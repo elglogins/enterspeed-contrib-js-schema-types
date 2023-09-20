bb0b4b19031ac840497d4c3cf4d7d190eedfbc25
elglogins <elo@enterspeed.com>
2023-09-20 11:58:13 +0200

    Dynamic configuration options for destinations

--- src/actions/destinations/context/context-methods/destination-by-alias.d.ts
+++ src/actions/destinations/context/context-methods/destination-by-alias.d.ts
@@ -1,4 +1,14 @@
+import { Nullable } from '../../../../types';
 import { IDestinationBuilder } from '../destinations';
 
 export interface IDestinationByAliasBuilder extends IDestinationBuilder {
+    /**
+     * Option to specify dynamic configuration for destination provider. 
+     * @param options 
+     * @example Example of destination - with options
+     * ```js
+     * context.destination('algolia').options({ customId: `prefixed-${sourceEntity.originId}` });
+     * ```
+     */
+    options(options: Nullable<Record<string, unknown>>): this
 }
